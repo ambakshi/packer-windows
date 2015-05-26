fad0be333a2974c5fec6332425f7dbf21359de28
Amit Bakshi <ambakshi@gmail.com>
2015-05-26 08:48:21 -0700

    Fix json to enable rdp and disable windows update

--- scripts/puppet-enterprise.bat
+++ scripts/puppet-enterprise.bat
@@ -1,10 +1,28 @@
-if not exist "C:\Windows\Temp\puppet.msi" (
-  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://pm.puppetlabs.com/puppet-enterprise/3.0.1/puppet-enterprise-3.0.1.msi', 'C:\Windows\Temp\puppet.msi')" <NUL
+if exist "C:\Windows\Temp\puppet.msi" exit 0
+
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    set URL=https://s3.amazonaws.com/pe-builds/released/3.8.0/puppet-enterprise-3.8.0-x64.msi
+) else (
+    set URL=https://s3.amazonaws.com/pe-builds/released/3.8.0/puppet-enterprise-3.8.0.msi
 )
 
-:: http://docs.puppetlabs.com/pe/latest/install_windows.html
-msiexec /qn /i C:\Windows\Temp\puppet.msi /log C:\Windows\Temp\puppet.log
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%URL%', 'C:\Windows\Temp\puppet.msi')" <NUL
+if errorlevel 1 goto :ErrorExit
 
-<nul set /p ".=;C:\Program Files (x86)\Puppet Labs\Puppet Enterprise\bin" >> C:\Windows\Temp\PATH
-set /p PATH=<C:\Windows\Temp\PATH
-setx PATH "%PATH%" /m
+:HavePuppetMSI
+:: http://docs.puppetlabs.com/pe/latest/install_windows.html
+start "puppet-install" /w msiexec /qn /i C:\Windows\Temp\puppet.msi /log C:\Windows\Temp\puppet.log
+if errorlevel 1 goto :ErrorExit
+
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    <nul set /p ".=;C:\Program Files\Puppet Labs\Puppet\bin" >> C:\Windows\Temp\PATH
+) else (
+    <nul set /p ".=;C:\Program Files (x86)\Puppet Labs\Puppet\bin" >> C:\Windows\Temp\PATH
+)
+set /p Path=<C:\Windows\Temp\PATH
+setx Path "%Path%" /m
+exit /b %ERRORLEVEL%
+
+:ErrorExit
+del /f /q C:\Windows\Temp\puppet.msi
+exit /b 1
